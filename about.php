170e4e1f5cf57c41eee97066c5946725d1451b3d
Trickyj <pop.tricky@gmail.com>
2017-06-27 11:58:57 +0530

    added all pages to my project

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fusion -- about us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="_/components/css/bootstrap.css">
+	<link rel="stylesheet" type="tex/css" href="_/components/css/myscript.css">
+</head>
+<body id="about">
+
+	<section class="container">
+		<div class="content row">
+			<section class="main col col-lg-8">
+				
+				</section><!-- main -->
+
+				<section class="sidebar col col-lg-4">
+			
+			</section><!-- sidebar -->
+		</div><!-- content -->
+	</section><!-- container -->	
+
+	<script type="text/javascript" src="_/components/js/bootstrap.js"></script>
+	<script type="text/javascript" src="_/components/js/myscript.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Web Development Project 1</title>
+	<title>Fusion -- home</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="_/components/css/bootstrap.css">
 	<link rel="stylesheet" type="tex/css" href="_/components/css/myscript.css">
 </head>
-<body>
-	<h1>Hello, vicky!</h1>
+<body id="home">
+
+	<section class="container">
+		<div class="content row">
+			<section class="main col col-lg-8">
+				
+				</section><!-- main -->
+
+				<section class="sidebar col col-lg-4">
+			
+			</section><!-- sidebar -->
+		</div><!-- content -->
+	</section><!-- container -->	
+
 	<script type="text/javascript" src="_/components/js/bootstrap.js"></script>
 	<script type="text/javascript" src="_/components/js/myscript.js"></script>
 </body>
