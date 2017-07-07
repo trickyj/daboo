ad5ed69fae2bebb1f2ef238562d54422f5b05fbb
Trickyj <pop.tricky@gmail.com>
2017-07-07 13:00:45 +0530

    no changes done

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Fusion -- home</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="_/components/css/bootstrap.css">
-	<link rel="stylesheet" type="tex/css" href="_/components/css/myscript.css">
+	<link rel="stylesheet" type="tex/css" href="_/components/css/_mystyles.less">
 </head>
 <body id="home">
 	<section class="container">
@@ -18,6 +18,9 @@
 	</section><!-- container -->	
 
 	<script type="text/javascript" src="_/components/js/bootstrap.js"></script>
-	<script type="text/javascript" src="_/components/js/myscript.js"></script>
+	<script type="text/javascript" src="_/components/js/dropdown.js"></script>
+	<script type="text/javascript" src="_/components/js/modal.js"></script>
 </body>
 </html>
+
+
