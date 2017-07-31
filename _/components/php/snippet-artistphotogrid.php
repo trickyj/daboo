5901e988b3483e0e4f50d9597db2d46fa16a0619
Trickyj <pop.tricky@gmail.com>
2017-07-31 10:37:59 +0530

    added about artists page, photogrid page, artist image

---
+++ _/components/php/snippet-artistphotogrid.php
@@ -0,0 +1,2 @@
+<div class="component"></div>
+<a href="artist.php#pallavi_dalvi"><img src="images/artists/pallavi_dalvi_tn.png" alt="photo of pallavi_dalvi"></a>
--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 			<?php include "_/components/php/snippet-carousel.php"; ?>
 		<section class="main col col-lg-8">  
 			<?php include "_/components/php/article-intro.php"; ?>
+			<?php include "_/components/php/article-abouttheartists.php"; ?>
 		</section> <!-- main -->
 
 		<section class="sidebar col col-lg-4">
@@ -21,7 +22,7 @@
 		</div><!-- content -->
 			<?php include "_/components/php/footer.php"; ?>
 		</section><!-- containter -->	
-
+ 
 	<script type="text/javascript" src="_/components/js/jquery.js"></script>
 	<script type="text/javascript" src="_/components/js/bootstrap.js"></script>
 	<script type="text/javascript" src="_/components/js/modal.js"></script>
