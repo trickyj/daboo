b9862241419792ba8437af7c380e4f2de350522b
Trickyj <pop.tricky@gmail.com>
2017-08-09 08:15:27 +0530

    accordian aside updated

--- _/components/php/aside-accordian.php
+++ _/components/php/aside-accordian.php
@@ -1,6 +1,26 @@
 <aside class="schedule">
 	<h2>Schedule</h2>
 	<p>There's a lot happening at our many workshop and talks. Make sure you check out the <a href="schedule.php">full schedule</a> for more information</p>
-<div class="accordion"></div>
+<div class="accordion" id="schedule">
+	
+<!-- Monday -->
+<section class="accordion-group">
+	<div class="accordion-heading">
+		<a href="#monday" class="accordion-toggle" data-toggle="collapse" data-parent="#schedule"><span class="glyphicon glyphicon-claendar"></span> Monday</a>
+	</div><!-- accordion-heading -->
+</section><!-- accordion-group -->
+
+<div id="monday" class="accordion-body collapse">
+<section class="accordion-inner">
+	<p> Some text goes here </p>
+	<p> <a href="schedule.php#monday" class="btn btn-danger" btn-small">Monday's Schedule</a></p>
+	</section><!-- accortion-inner -->
+</div>
+
+<secontion class="testcomment">
+	
+</secontion>
+
+</div>
 
 </aside>
